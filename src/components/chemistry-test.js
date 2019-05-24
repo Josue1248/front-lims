c7a77ce4ef8a690852877655ccd68c67148c54d8
Josue Montaño <jmontano@biosoftinc.com>
2019-05-24 14:11:15 -0700

    Adding modal and changing chem error message

--- src/components/chemistry-test.js
+++ src/components/chemistry-test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+import Modal from './modal.js';
+
 export default class ChemistryTest extends React.Component{
     constructor(props){
         super(props);
@@ -17,6 +19,7 @@ export default class ChemistryTest extends React.Component{
             messageSample: '',
             validSample: undefined,
             messageAPI: '',
+            showModal: false,
             loading: false,
         }
         this.handleSample = this.handleSample.bind(this);
@@ -28,7 +31,9 @@ export default class ChemistryTest extends React.Component{
         this.handleChemistry = this.handleChemistry.bind(this);
         this.handleChemistryValidation = this.handleChemistryValidation.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.hideModal = this.hideModal.bind(this);
     }
+
     handleSample(e) {
         const sample = e.target.value
 
@@ -136,14 +141,14 @@ export default class ChemistryTest extends React.Component{
                     })
                 }
             })
-        } else if(chemistry === '') {
+        } else if(chemistry.trim() === '') {
             this.setState({
                 messageCh: '',
                 validCh: false,
             })
         } else {
             this.setState({
-                messageCh: 'Invalid syntax',
+                messageCh: 'Error de syntaxis',
                 validCh: false,
             })
         }
@@ -171,28 +176,37 @@ export default class ChemistryTest extends React.Component{
             states: ["Prueba de quimica pasada", "Muestra lista para prueba de centrifugado"]
         })
         .then( res => {
-            if(res.data.message) {
-				console.log(res.data.message)
+            if(res.data.success === true) {
                 this.setState({
                     sample: '',
                     messageAPI: res.data.message,
                     validSample: false,
+                    showModal: true,
                     loading: false,
 				});
 				ReactDOM.findDOMNode(this.refs.firstSample).focus();
             } else {
 				this.setState({
+                    messageAPI: res.data.message,
+                    showModal: true,
 					loading: false,
 				})
 			}
         })
         .catch( () => {
-            alert('Conection Timed Out');
-            this.setState({
-                loading: false
-            });
+			this.setState({
+                messageAPI: 'Fallo en la conexion',
+                showModal: true,
+				loading: false
+			});
         });
     }
+
+    hideModal() {
+        this.setState({ 
+            showModal: !this.state.showModal 
+        });
+    };
 
     render(){
         const format = 'MU-##-#####'
@@ -206,63 +220,66 @@ export default class ChemistryTest extends React.Component{
             </div>
             <div className='col-sm-12 col-xl-10'>
                 <form onSubmit={this.handleSubmit}>
-                <div className='row justify-content-center form-inline mb-3'>
-                        <label className={regularLabels}>Operador:</label>
-                        <input 
-                            type='text' 
-                            value={this.state.operator}
-                            className={inputs}
-                            name='operator' 
-                            placeholder='#####'
-                            onChange={this.handleOperator}
-                        />
-                        <label className={warningLabels}>{this.state.messageOp}</label>
-                    </div>
                     <div className='row justify-content-center form-inline mb-3'>
-                        <label className={regularLabels}>Químico:</label>
-                        <input 
-                            type='text' 
-                            value={this.state.chemistry}
-                            className={inputs}
-                            name='chemistry'
-                            placeholder='QU-#####'
-                            onChange={this.handleChemistry}
-                            onBlur={this.handleChemistryValidation}
-                        />
-                        <label className={warningLabels}>{this.state.messageCh}</label>
-                    </div>
+                            <label className={regularLabels}>Operador:</label>
+                            <input 
+                                type='text' 
+                                value={this.state.operator}
+                                className={inputs}
+                                name='operator' 
+                                placeholder='#####'
+                                onChange={this.handleOperator}
+                            />
+                            <label className={warningLabels}>{this.state.messageOp}</label>
+                        </div>
+                    <div className='row justify-content-center form-inline mb-3'>
+                            <label className={regularLabels}>Químico:</label>
+                            <input 
+                                type='text' 
+                                value={this.state.chemistry}
+                                className={inputs}
+                                name='chemistry'
+                                placeholder='QU-#####'
+                                onChange={this.handleChemistry}
+                                onBlur={this.handleChemistryValidation}
+                            />
+                            <label className={warningLabels}>{this.state.messageCh}</label>
+                        </div>
                     <div>
                         <h5 className='text-center m-4'>Código</h5>
-                    <div className='row justify-content-center form-inline mb-3'>
-                        <label className={regularLabels}>Muestra 1:</label>
-                        <input 
-                            type='text'
-                            className={inputs}
-                            name={'sample1'}
-                            value={this.state.sample}
-                            placeholder={format}
-                            onBlur={this.handleOnBlur}
-                            onChange={this.handleSample}
-							ref='firstSample'
-                        />
-                        <label className={warningLabels}>{this.state.messageSample}</label> 
+                        <div className='row justify-content-center form-inline mb-3'>
+                            <label className={regularLabels}>Muestra 1:</label>
+                            <input 
+                                type='text'
+                                className={inputs}
+                                name={'sample1'}
+                                value={this.state.sample}
+                                placeholder={format}
+                                onBlur={this.handleOnBlur}
+                                onChange={this.handleSample}
+                                ref='firstSample'
+                            />
+                            <label className={warningLabels}>{this.state.messageSample}</label> 
+                        </div>
                     </div>
-                    </div>
-					<div className='row justify-content-center'>
-                    <label className={'col-lg-3 col-sm-10 text-center col-md-6  mt-3'}><p id='success'>{this.state.messageAPI}</p></label>
-					</div>
                     <div className='row justify-content-center'>
-                    <button
-                        type='submit'
-                        className='btn button col-md-6 col-sm-10 col-lg-3'
-                        disabled={(/\d{5}/g.test(this.state.operator) && this.state.validCh && this.state.validSample) ? false : true}
-                        title={(this.state.validSamples && this.state.validCh && /\d{5}/g.test(this.state.operator)) ? 'La forma esta lista' : 'La forma no esta lista'}
-                    >
-                    Guardar
-                    {(this.state.loading) ? <img src='/images/spinner.gif' alt='loading' id='spinner'/> : ''}
-                    </button>
+                        <button
+                            type='submit'
+                            className='btn button col-md-6 col-sm-10 col-lg-3'
+                            disabled={(/\d{5}/g.test(this.state.operator) && this.state.validCh && this.state.validSample) ? false : true}
+                            title={(this.state.validSamples && this.state.validCh && /\d{5}/g.test(this.state.operator)) ? 'La forma esta lista' : 'La forma no esta lista'}
+                        >
+                        Guardar
+                        {(this.state.loading) ? <img src='/images/spinner.gif' alt='loading' id='spinner'/> : ''}
+                        </button>
                     </div>
                 </form>
+                <Modal 
+                    showModal={this.state.showModal} 
+                    handleClose={this.hideModal}
+                    title={this.state.name}
+                    message={this.state.messageAPI}
+                />
             </div>
         </div>)
     }
