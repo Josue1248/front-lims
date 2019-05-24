2e598752865c5c0bb8be6b74e0c4b664564a8ea8
Josue Montaño <jmontano@biosoftinc.com>
2019-05-24 14:12:28 -0700

    Adding modal component

---
+++ src/components/modal.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class Modal extends React.Component{
+
+    handleClose = () => {
+        this.props.handleClose()
+    };
+
+    render(){
+        const {
+            handleClose,
+            props:{
+                title,
+                message,
+                showModal,
+            }
+          } = this;
+
+        const showHideClassName = showModal ? "modal display-block" : "modal display-none";
+    
+        return (
+        <div className={showHideClassName} tabIndex="-1" role="dialog" aria-hidden={!showModal}>
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h4 className="modal-title">{title}</h4>
+                    </div>
+                    <div className="modal-body">
+                    <p className="text-center">{message}</p>
+                    </div>
+                    <div className="modal-footer">
+                        <button className={"btn button"}onClick={handleClose}>Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default Modal;
